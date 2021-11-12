63ad015fab9403540e7a407c8edb2165d89c3f6a
liuSirli <94122720+liuSirli@users.noreply.github.com>
2021-11-12 20:14:41 +0800

    Update GitTest.java
    pull

--- src/main/java/com/xxx/GitTest.java
+++ src/main/java/com/xxx/GitTest.java
@@ -11,5 +11,6 @@ public class GitTest {
         System.out.println("master test");
         System.out.println("hot-fix test");
         System.out.println("remote ssh push");
+        System.out.println("remote ssh pull");
     }
 }
